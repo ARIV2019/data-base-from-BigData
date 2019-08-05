b444bc74d422b8b3537dd7ee0f04415c4a8cbb0e
ARIV2019 <cdban7@yandex.ru>
2019-08-05 15:07:09 +0300

    задание 3 урока к 3 и 4 темам

---
+++ topik3_2.sql
@@ -0,0 +1,36 @@
+/*Таблица users была неудачно спроектирована. 
+Записи created_at и updated_at были заданы типом VARCHAR и в них долгое время 
+помещались значения в формате "20.10.2017 8:10". 
+Необходимо преобразовать поля к типу DATETIME, сохранив введеные ранее значения.
+*/
+
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) COMMENT 'Имя покупателя',
+  birthday_at DATE COMMENT 'Дата рождения',
+  created_at VARCHAR(255) COMMENT 'Дата создания',
+  updated_at VARCHAR(255) COMMENT 'Дата обновления'
+) COMMENT = 'Покупатели';
+
+INSERT INTO users (name, birthday_at, created_at, updated_at) VALUES
+  ('Геннадий', '1990-10-05', '20.10.2017 8:10', '20.10.2017 8:10'),
+  ('Наталья', '1984-11-12', '20.10.2017 8:10', '20.10.2017 8:10'),
+  ('Александр', '1985-05-20', '20.10.2017 8:10', '20.10.2017 8:10'),
+  ('Сергей', '1988-02-14', '20.10.2017 8:10', '20.10.2017 8:10'),
+  ('Иван', '1998-01-12', '20.10.2017 8:10', '20.10.2017 8:10'),
+  ('Мария', '1992-08-29', '20.10.2017 8:10', '20.10.2017 8:10');
+
+AlTER TABLE users ADD new_created_at DATETIME;
+AlTER TABLE users ADD new_updated_at DATETIME;
+
+UPDATE users SET new_created_at=CAST(created_at AS DATETIME) WHERE id>0;
+UPDATE users SET new_created_at=CAST(created_at AS DATETIME) WHERE id>0;
+
+select * from users;
+
+
+
+
+ 
+    
